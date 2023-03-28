305e185d681075dbea2228883c62539d24d8bb94
CT56567 <126840265+CT56567@users.noreply.github.com>
2023-03-29 08:46:33 +1100

    Create pleasy.sh

--- pleasy.sh
+++ pleasy.sh
@@ -2,6 +2,7 @@ sudo apt update
 sudo apt upgrade -y
 cd $home
 git clone https://github.com/mayostudio/pleasy.git
-sudo bash ./pleasy/bin/pl init
+cd $home
+bash ./pleasy/bin/pl init
 sudo echo "PATH=$Path:$home/pleasy/bin" >> ~/.bashrc 
 
