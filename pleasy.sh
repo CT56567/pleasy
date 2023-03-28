b166d52af9f05712e16b0a4479126612e71bfd3a
CT56567 <126840265+CT56567@users.noreply.github.com>
2023-03-28 14:51:51 +1100

    Update pleasy.sh

--- pleasy.sh
+++ pleasy.sh
@@ -3,5 +3,4 @@ sudo apt upgrade -y
 cd $home
 git clone https://github.com/mayostudio/pleasy.git
 sudo bash ./pleasy/bin/pl init
-sudo echo "PATH=$Path:$home/pleasy/bin" >> ~/.bashrc 
-
+sudo echo "export PATH=$home/pleasy/bin/:$PATH" >> $home/.bashrc 
