e85926a897e29214e3429dadb2cf071c38d4a2b9
rjzaar <rob@cathnet.org>
2023-05-23 13:09:54 +1000

    "Fix 216"

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 prod_alias="opencat"
+step=4
+
+if[ "$step" -lt "4" ]; then
+  echo "is less than"
+else
+  echo "not less than"
+  fi
+
    prod_backups=$(ssh $prod_alias "cd /home/opencat/opencat.org/ && echo \"($(find -maxdepth 1 -name "*.sql" -print0 | xargs -0 ls -1 -t))\" ")
    Name=($prod_backups)
    echo ${Name:3}
